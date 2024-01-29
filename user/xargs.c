df5d6f579210d3c5c126e52518246c92ea871246
zixiangcode <zixiangcode@gmail.com>
2024-01-29 15:25:55 +0800

    [Fix]Refactor xargs.c for clarity.

--- user/xargs.c
+++ user/xargs.c
@@ -8,8 +8,8 @@ command.
 
 int main(int argc, char *argv[]) {
     char *args[MAXARG];  // 存放 xargs 命令的参数
-    char buf[512];
-    int n;
+    char buf[512], temp[512];
+    int n, tempIndex = 0, argIndex = argc - 1;
     if (argc < 2) {
         fprintf(2, "Usage: xargs command\n");
         exit(1);
@@ -18,13 +18,23 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         args[i - 1] = argv[i];
     }
+    args[argc - 1] = temp;
+    args[argc] = 0; // 设置 NULL 作为结尾
     // 从标准输入读取一行，作为 xargs 命令的参数
     while ((n = read(0, buf, sizeof(buf))) > 0) {
-        char temp[512] = {"\0"};
-        args[argc - 1] = temp;
         for (int i = 0; i < strlen(buf); i++) {
             // 碰到换行符，就 fork 一个子进程执行 xargs 命令
             if (buf[i] == '\n') {
+                // 碰到换行符了，结束当前行
+                temp[tempIndex] = '\0';
+                // 检查参数是否过多
+                if (argIndex < MAXARG - 1) {
+                    args[argIndex++] = temp;
+                } else {
+                    fprintf(2, "Xargs: Too many arguments\n");
+                    exit(1);
+                }
+
                 if (fork() == 0) {
                     if (exec(argv[1], args) == -1) {
                         fprintf(2, "exec failed\n");
@@ -33,8 +43,14 @@ int main(int argc, char *argv[]) {
                 } else {
                     wait(0);
                 }
+
+                // 重置 temp 和 argIndex 为下一行准备
+                tempIndex = 0;
+                argIndex = argc - 1;
+                args[argIndex] = temp;
+
             } else {
-                temp[i] = buf[i];
+                temp[tempIndex++] = buf[i];
             }
         }
     }
